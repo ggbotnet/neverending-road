311f42fe4258a346511e2ca0fe3082eaf5f79f73
GGBotNet <59578306+ggbotnet@users.noreply.github.com>
2025-02-28 18:36:15 +0200

    godot-4

---
+++ Godot Engine 4/scripts/GameOver.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+@onready var select = $SpinBox
+@onready var ui_hisocre = $C/VBoxC/HI
+@onready var ui_score = $C/VBoxC/SCORE
+@onready var ui_contiune = $C/VBoxC/Continue
+@onready var ui_end = $C/VBoxC/End
+
+func _ready():
+	if Global.score > Global.high_score:
+		Global.high_score = Global.score
+	ui_score.text = "SCORE " + str(Global.score)
+	ui_hisocre.text = "HI " + str(Global.high_score)
+
+func _physics_process(_delta):
+	var textcolor = Color("626262")
+	var textcolorsel = Color("FFFFFF")
+	if select.value == 1:
+		ui_contiune.set("theme_override_colors/font_color",textcolorsel)
+		ui_end.set("theme_override_colors/font_color",textcolor)
+		if Input.is_action_just_pressed("keyboard_enter"):
+			# warning-ignore:return_value_discarded
+			get_tree().change_scene_to_file("res://scenes/Game.tscn")
+			Global.Reset()
+	if select.value == 2:
+		ui_contiune.set("theme_override_colors/font_color",textcolor)
+		ui_end.set("theme_override_colors/font_color",textcolorsel)
+		if Input.is_action_just_pressed("keyboard_enter"):
+			# warning-ignore:return_value_discarded
+			get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+
+func _input(event):
+	if event.is_action_pressed("keyboard_esc"):
+		get_tree().quit()
+	if event.is_action_pressed("keyboard_down"):
+		select.value = 2
+	if event.is_action_pressed("keyboard_up"):
+		select.value = 1
---
+++ Godot Engine 4/scripts/MainMenu.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+var scene_game = preload("res://scenes/Game.tscn")
+@onready var ui_newgame = $Control/VBoxC/NewGame
+@onready var ui_exit = $Control/VBoxC/Exit
+@onready var select = $SpinBox
+@onready var hiscore = $Control/VBoxC/HiScore
+
+func _ready():
+	if Global.score > Global.high_score:
+		Global.high_score = Global.score
+	hiscore.text = "HI-SCORE " +str(Global.high_score)
+
+func _physics_process(_delta):
+	var textcolor = Color("626262")
+	var textcolorsel = Color("FFFFFF")
+	if select.value == 1:
+		ui_newgame.set("theme_override_colors/font_color",textcolorsel)
+		ui_exit.set("theme_override_colors/font_color",textcolor)
+		if Input.is_action_just_pressed("keyboard_enter"):
+			# warning-ignore:return_value_discarded
+			get_tree().change_scene_to_packed(scene_game)
+			Global.Reset()
+	if select.value == 2:
+		ui_newgame.set("theme_override_colors/font_color",textcolor)
+		ui_exit.set("theme_override_colors/font_color",textcolorsel)
+		if Input.is_action_just_pressed("keyboard_enter"):
+			get_tree().quit()
+
+func _input(event):
+	if event.is_action_pressed("keyboard_esc"):
+		get_tree().quit()
+	if event.is_action_pressed("keyboard_down"):
+		select.value = 2
+	if event.is_action_pressed("keyboard_up"):
+		select.value = 1
